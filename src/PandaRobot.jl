007f84922b5b0cfce6b0fe74b0680f4f14830a8d
taoyao <19646883+taohnouaccountb@users.noreply.github.com>
2020-04-10 16:44:49 -0700

    Automatically load package path to ROS_PACKAGE_PATH

--- src/PandaRobot.jl
+++ src/PandaRobot.jl
@@ -26,6 +26,10 @@ function __init__()
   if !isfile(urdfpath())
     error("Could not find $(urdfpath()). Please run `import Pkg; Pkg.build(\"Panda\")`.")
   end
+  
+  # Setup ros environment to help MeshCatMechanism.jl to resolve "package://..." in the urdf file
+  PandaRobotDepsPath = string(dirname(pathof(PandaRobot)), "/../deps/")
+  ENV["ROS_PACKAGE_PATH"] = string(get(ENV, "ROS_PACKAGE_PATH", ""), ":", PandaRobotDepsPath)
 end
 
 end # module
